114540a05c34cb68d7a1e157b1791d3ee0767639
Abdullah Alshibily <abdullah.alshibily@luminoslabs.com>
2022-08-05 16:35:46 -0500

    Test setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import '@styles/globals.css'
 
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'GTM-5CWTVQ2'
+}
+
+
+
 function Application({ Component, pageProps }) {
+  TagManager.initialize(tagManagerArgs)
   return <Component {...pageProps} />
 }
 
