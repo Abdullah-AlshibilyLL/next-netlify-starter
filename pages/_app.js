86c3c0b2a0a45f8e43a3c4634054e73f36decdcc
Abdullah Alshibily <abdullah.alshibily@luminoslabs.com>
2022-08-05 17:14:01 -0500

    gtm testing #1

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '@styles/globals.css'
+import { Script } from 'next/script'
 
 import TagManager from 'react-gtm-module'
 
@@ -8,9 +9,31 @@ const tagManagerArgs = {
 
 
 
-function Application({ Component, pageProps }) {
-  TagManager.initialize(tagManagerArgs)
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${tagManagerArgs.gtmId}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${tagManagerArgs.gtmId}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default Application
+export default App
