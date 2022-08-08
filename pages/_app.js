437c69ceb359ae05f2da2bd252c780364485d0bb
Abdullah Alshibily <abdullah.alshibily@luminoslabs.com>
2022-08-08 09:18:27 -0500

    Fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,17 @@
 import '@styles/globals.css'
 
+import { useEffect } from 'react'
+import TagManager from 'react-gtm-module'
+
 // const tagManagerArgs = {
 //   gtmId: 'GTM-5CWTVQ2'
 // }
 
 function App ({ Component, pageProps }){
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  )
+  useEffect(() => {
+    TagManager.initialize({gtmId: "GTM-5CWTVQ2"});
+  }, []);
+  return <Component {...pageProps} />
 }
 
 export default App
