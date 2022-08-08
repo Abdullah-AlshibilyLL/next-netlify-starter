d5ad18561b612fc4c7cce33db342347b29dc6b98
Abdullah Alshibily <abdullah.alshibily@luminoslabs.com>
2022-08-08 08:32:47 -0500

    GTM Fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,12 @@
 import '@styles/globals.css'
-import { Script } from 'next/script'
 
-import TagManager from 'react-gtm-module'
+// const tagManagerArgs = {
+//   gtmId: 'GTM-5CWTVQ2'
+// }
 
-const tagManagerArgs = {
-  gtmId: 'GTM-5CWTVQ2'
-}
-
-
-
-const App = ({ Component, pageProps }) => {
+function App ({ Component, pageProps }){
   return (
     <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${tagManagerArgs.gtmId}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${tagManagerArgs.gtmId}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <Component {...pageProps} />
     </>
   )
