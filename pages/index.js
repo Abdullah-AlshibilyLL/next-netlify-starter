4c25fe8fc1af0bc101abaa9c2a14edffae284d8b
Abdullah Alshibily <abdullah.alshibily@luminoslabs.com>
2022-08-08 09:18:19 -0500

    GTM Correctly Implemented

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <GTM />
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
